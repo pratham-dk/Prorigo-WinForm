e8b4daef903f8a35fb9ced7b1fe1802a147aeb60
pratham-dk <prathamesh.dixit.88@gmail.com>
2022-05-05 15:14:04 +0530

    hello

--- WinFormsApp/day05052022/FormForFile.cs
+++ WinFormsApp/day05052022/FormForFile.cs
@@ -55,7 +55,7 @@ namespace WinFormsApp.day05052022
                 else
                 {
                    File.Create(path);
-                    MessageBox.Show("File Created Hello ");
+                    MessageBox.Show("File Created ");
                 }
             }
             catch (Exception ex)
